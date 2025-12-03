962b9736d779c1caf3aa324bd6ad01e315d8b4cc
jerezoff <jerezoff@gmail.com>
2025-12-03 10:36:35 +0500

    added rnote

--- home/home.nix
+++ home/home.nix
@@ -4,7 +4,7 @@
   home.homeDirectory = "/home/jerezoff";
   home.stateVersion = "25.05";
 
-  home.packages = with pkgs; [ kubectl k9s git tmux wofi ghostty firefox ];
+  home.packages = with pkgs; [ kubectl k9s git tmux wofi ghostty firefox rnote ];
 
   programs.home-manager.enable = true;
 
