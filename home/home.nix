9102acd12dcae8df2d9866e2edb9c7f4ab8d9625
jerezoff <jerezoff@gmail.com>
2025-12-11 18:04:41 +0500

    removed rnote

--- home/home.nix
+++ home/home.nix
@@ -4,7 +4,7 @@
   home.homeDirectory = "/home/jerezoff";
   home.stateVersion = "25.11";
 
-  home.packages = with pkgs; [ kubectl k9s git tmux wofi ghostty firefox rnote ];
+  home.packages = with pkgs; [ kubectl k9s git tmux wofi ghostty firefox ];
 
   programs.home-manager.enable = true;
 
