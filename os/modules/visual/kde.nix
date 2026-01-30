4e1956e5aa3c9876a487c038623f673edf3e65b8
jerezoff <jerezoff@gmail.com>
2026-01-30 19:48:43 +0500

    fix

--- os/modules/visual/kde.nix
+++ os/modules/visual/kde.nix
@@ -1,7 +1,7 @@
 {...}: {
   services = {
     desktopManager.plasma6.enable = true;
-    displayManager.sddm.enable = true;
-    displayManager.sddm.wayland.enable = true;
+    desktopManager.sddm.enable = true;
+    desktopManager.sddm.wayland.enable = true;
   };
 }
